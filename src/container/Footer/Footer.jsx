e55a1473fc6bf2bb2eb7acc102d7ee75799e0a3e
Rique2 <enrique2heinrich@gmail.com>
2023-05-11 18:12:38 +0200

    Footer

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div class="footer">
+      <div className="container-footer">
+        <p>Enrique Hoebeb © Copyright 2023</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
