e37c4c5629ca551aba96d523dabdfb38bc4ed3a3
Rique2 <enrique2heinrich@gmail.com>
2023-05-13 02:18:23 +0200

    Add gifs and png

--- src/container/Slider/SwiperSlideContent.js
+++ src/container/Slider/SwiperSlideContent.js
@@ -3,7 +3,10 @@ import { motion } from "framer-motion";
 import { RiFlutterFill } from "react-icons/ri";
 import { SiDart } from "react-icons/si";
 import ReusableButton from "../../components/Buttons/ReusableButton";
-
+import shoppingCart from "../../assets/shopping-cart.gif";
+import Battleship from "../../assets/battleship.png";
+import WeatherApp from "../../assets/weatherApp.png";
+import MemoryGame from "../../assets/memoryGame.gif";
 
 const SwiperSlideContent = (props) => {
   const { project1, project2, project3, project4, project5 } = props;
@@ -15,21 +18,25 @@ const SwiperSlideContent = (props) => {
 
   if (project1) {
     name = "Shopping-Cart";
+    image = shoppingCart;
     project = project1;
     link = "https://github.com/Rique2x/React-Shopping-Cart";
     view = "https://rique2x.github.io/React-Shopping-Cart";
   } else if (project2) {
     project = project2;
     name = "Battleship";
+    image = Battleship;
     link = "https://github.com/Rique2x/Battleship";
     view = "https://rique2x.github.io/Battleship";
   } else if (project3) {
     project = project3;
+    image = MemoryGame;
     name = "Memory-Card";
     link = "https://github.com/Rique2x/React-Memory-Card";
     view = "https://rique2x.github.io/React-Memory-Card";
   } else if (project4) {
     project = project4;
+    image = WeatherApp;
     name = "Weather-App";
     link = "https://github.com/Rique2x/Weather-App";
     view = "https://rique2x.github.io/Weather-App";
