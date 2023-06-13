d43315afc5606862cfef21cd629482cad3e56dbb
Enrique H.H <enrique2heinrich@gmail.com>
2023-06-13 12:36:30 +0200

    Update About.jsx

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -17,23 +17,21 @@ const About = () => {
           <h1>About Me</h1>
           <p className="title-text">
           Hi, my name is Enrique Hoebeb and I am a 22-year-old self-taught 
-          full-stack developer who embarked on my programming journey just 
-          9 months ago through The Odin Project. Hailing from the beautiful 
-          town of Arandis, Namibia, I have shown remarkable dedication and 
-          passion in my pursuit of learning to code. Thanks to The Odin Project, 
-          I have developed a solid foundation in web development and have been 
-          working diligently to improve my skills in both front-end and back-end 
-          development.
+          full-stack developer who embarked on my programming journey through The 
+          Odin Project. Originally from the beautiful town of Arandis, Namibia, 
+          but residing in Canada I have shown remarkable dedication and passion in 
+          my pursuit of learning to code. Thanks to The Odin Project,I have developed 
+          a solid foundation in web development and have been working diligently to 
+          improve my skills in both front-end and back-end development.
           </p>
           <p className="title-text">
           My passion lies in programming, new technology, and all things IT-related. 
           I am always eager to tackle new challenges, and if you have any job opportunities 
           you'd like to discuss with me for the summer or fall of 2023, please don't 
-          hesitate to reach out. This website serves as a complement to my CV, so feel
-          free to check that out as well.
+          hesitate to reach out. 
           </p>
           <p className="title-text">
-          Additionally, I am available for gigs and freelancing projects. If you have any 
+          I am available for gigs and freelancing projects. If you have any 
           short-term assignments or specific tasks that require programming expertise, 
           I'm more than happy to discuss them with you. Whether it's a small project or a 
           one-time task, I'm open to considering various opportunities. Please don't hesitate 
